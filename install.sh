3e4caf0856a795d860369cb8a16941e91379512f
atomicul <soaremihai2006@gmail.com>
2024-12-15 00:42:04 +0200

    test #1

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+
+# Install .bashrc
+cp ./.bashrc "$HOME/.bashrc"
+
+# Install nerd font
+mkdir -p /tmp/roboto-mono
+mkdir -p /usr/local/share/fonts/RobotoMono
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/RobotoMono.zip" -O /tmp/roboto-mono/RobotoMono.zip
+unzip /tmp/roboto-mono/RobotoMono.zip -d /usr/local/share/fonts/RobotoMono
+
+# Install usual applications
+apt-get install -y xfce4-terminal discord steam lutris google-chrome-stable spotify-client gimp
+
+# Configure i3wm
+apt-get install -y i3
+
+## i3/config dependencies
+apt-get install -y redshift feh playerctl
+
+readarray -t secondary_monitors <<<$(xrandr | grep " connected " | grep -v " primary " | awk '{ print$1 }')
+primary_monitor=$(xrandr | grep " primary " | awk '{ print$1 }')
+
+sed -E "$(
+	printf '%s%s\n' 's/^(set \$primary-monitor ).*$' "/\\1\"${primary_monitor}\"/gm;t"
+)" ./i3/config >/tmp/i3-config
+
+if test ${#secondary_monitors[*]} -lt 1; then
+	sed -E -i 's/^(set \$secondary-monitor ).*$//gm;t' /tmp/i3-config
+	sed -E -i 's/^bindsym \$mod\+Ctrl\+[0-9].*$//gm;t' /tmp/i3-config
+else
+	sed -E -i "$(
+		printf '%s%s\n' 's/^(set \$secondary-monitor ).*$' "/\\1\"${secondary_monitors[0]}\"/gm;t"
+	)" /tmp/i3-config
+fi
+
+mkdir -p "$HOME/.config/i3"
+mv /tmp/i3-config "$HOME/.config/i3/config"
+cp ./i3/lock-screen.sh "$HOME/.config/i3"
+
+apt-get install -y i3status
+mkdir -p "$HOME/.config/i3status"
+cp ./i3status/config "$HOME/.config/i3status"
+
+apt-get install -y picom
+mkdir -p "$HOME/.config/picom"
+cp ./picom/picom.conf "$HOME/.config/picom"
+
+apt-get install -y rofi
+mkdir -p "$HOME/.config/rofi"
+cp ./rofi/config.rasi "$HOME/.config/rofi"
+
+## Install i3lock-color
+apt-get install -y autoconf gcc make pkg-config libpam0g-dev libcairo2-dev \
+	libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev \
+	libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev \
+	libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+
+git clone https://github.com/Raymo111/i3lock-color.git /tmp/i3lock-color
+bash /tmp/i3lock-color/install-i3lock-color.sh
+
+# Install tmux
+apt-get install -y tmux
