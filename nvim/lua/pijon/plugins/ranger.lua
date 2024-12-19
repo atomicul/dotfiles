9b33c9cf9b6a4dd0d73324e6425cad6764a3f5d4
atomicul <soaremihai2006@gmail.com>
2024-12-19 17:21:07 +0200

    Fix ranger error

--- nvim/lua/pijon/plugins/ranger.lua
+++ nvim/lua/pijon/plugins/ranger.lua
@@ -2,5 +2,4 @@ return {{
     'francoiscabrol/ranger.vim',
     dependencies={'rbgrouleff/bclose.vim'},
     keys={{'<leader>l', ":Ranger<cr>"}},
-    init=function() vim.keymap.remove('n', '<leader>f') end
 }}
