0deea3d0fe845ea9e638716942e4c1f584826080
atomicul <soaremihai2006@gmail.com>
2024-12-29 15:20:22 +0200

    Add docker daemonizer script

--- install.sh
+++ install.sh
@@ -158,3 +158,20 @@ sudo ln -s "$HOME/.config/nvim" /root/.config/nvim
 
 # Other packages
 sudo apt-get install -y btop bat xclip neofetch
+
+## docker
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## docker daemonizer
+mkdir -p "$HOME/.local/bin"
+cp ./daemonize-docker "$HOME/.local/bin"
+chmod +x "$HOME/.local/bin/daemonize-docker"
