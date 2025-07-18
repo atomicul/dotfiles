91ecd6eb8738980d2d4db58c0bb6c5f06544af8e
Mihai Soare <mihai.soare@netrom.live>
2025-07-18 15:10:33 +0300

    [neovim] Set tab width to differ from 4 spaces (display it as 8)

--- nvim/lua/pijon/init.lua
+++ nvim/lua/pijon/init.lua
@@ -9,7 +9,7 @@ vim.opt.termguicolors = true
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
