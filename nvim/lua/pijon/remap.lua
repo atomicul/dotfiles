d9815d2d558af10368079d8edf8ff103bc05470f
atomicul <soaremihai2006@gmail.com>
2025-11-26 15:47:25 +0200

    [neovim] Add comments for the more unclear remaps

--- nvim/lua/pijon/remap.lua
+++ nvim/lua/pijon/remap.lua
@@ -6,9 +6,11 @@ vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
 
 vim.keymap.set("n", "<leader>qq", ":q<CR>")
 
+-- Move lines by pressing J,K in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Keep search term centered while doing '/' searches
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
